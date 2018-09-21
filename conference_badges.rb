ad0c21b154805a025023bd6766dcb591fb6f9f7a
tgray017 <tgray017@gmail.com>
2018-09-21 01:24:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -52,8 +52,8 @@ end
 
 
 def printer(names_array)
-    puts batch_badge_creator(names_array[0])
-    puts assign_rooms(names_array[0])
+    puts batch_badge_creator(names_array)
+    puts assign_rooms(names_array)
 end
 
 # printer(["Tom", "Victoria", "Wilson", "Patrick", "Patti", "Duncan", "Cassandra"])
