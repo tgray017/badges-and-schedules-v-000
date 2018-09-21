0c37fc2437e4243702d040f47f9b47fbec24d8c3
tgray017 <tgray017@gmail.com>
2018-09-21 01:02:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,14 +26,14 @@ end
 
 # improved assign_rooms method
 
-  def assign_rooms_2(names_array)
-    room_assignments = []
-      names_array.each_with_index do |name, index|
-        room = index + 1
-        room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
-      end
-    return room_assignments
-  end
+#  def assign_rooms_2(names_array)
+#    room_assignments = []
+#      names_array.each_with_index do |name, index|
+#        room = index + 1
+#        room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
+#      end
+#    return room_assignments
+#  end
 
 
 # final assign_rooms method
